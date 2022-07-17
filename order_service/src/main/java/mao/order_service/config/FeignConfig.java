d0ffbc2485ff154e3fa1197c8041f21e370c91f7
mao <1296193245@qq.com>
2022-07-17 21:12:34 +0800

    spring_cloud_demo_Feign_unified_interface

---
+++ order_service/src/main/java/mao/order_service/config/FeignConfig.java
@@ -0,0 +1,31 @@
+package mao.order_service.config;
+
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Project name(项目名称)：spring_cloud_demo_Feign
+ * Package(包名): mao.order_service.config
+ * Class(类名): FeignConfig
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/7/17
+ * Time(创建时间)： 19:56
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+
+public class FeignConfig
+{
+//    @Bean
+//    public Logger.Level feignLoggerLevel()
+//    {
+//        return Logger.Level.FULL;
+//    }
+}
